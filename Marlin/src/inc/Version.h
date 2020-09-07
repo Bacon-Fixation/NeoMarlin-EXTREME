ef2f8c7694143a5392fc1d5ff691104153b589aa
Bacon-Fixation <baconfixation@gmail.com>
2020-09-07 11:41:23 -0500

    Update Version.h

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -33,7 +33,7 @@
  * vendor name, download location, GitHub account, etc.
  */
 #ifndef DETAILED_BUILD_VERSION
-  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
+  #define DETAILED_BUILD_VERSION "Marlin 2.0.6.2"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2020-09-03"
+  #define STRING_DISTRIBUTION_DATE "2020-09-07"
 #endif
 
 /**
